b41d03ec460325ac527ad2065212337fd8b89856
Arnaud Boudou <aboudou@ekito.fr>
2012-03-17 13:15:47 +0100

    Possibilité d'ajouter un contact hors carnet d'adresse

---
+++ What and Whom/IDContactViewController.h
@@ -0,0 +1,13 @@
+//
+//  IDContactViewController.h
+//  What and Whom
+//
+//  Created by Boudou Arnaud on 17/03/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IDContactViewController : UIView
+
+@end
---
+++ What and Whom/IDContactViewController.m
@@ -0,0 +1,31 @@
+//
+//  IDContactViewController.m
+//  What and Whom
+//
+//  Created by Boudou Arnaud on 17/03/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "IDContactViewController.h"
+
+@implementation IDContactViewController
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
